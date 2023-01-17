585084573ba0ec4354ee1b25dd6ec061ddf16583
Oksana <106753310+oksanamatsunych@users.noreply.github.com>
2023-01-17 23:51:57 +0000

    Create hometask2.js

---
+++ hometask2.js
@@ -0,0 +1,7 @@
+const numberAsString = "199";
+const number = 10;
+
+const numberFrom_numebrAsString = parseInt(numberAsString);
+
+const sum = numberFrom_numebrAsString + number;
+console.log(sum);
