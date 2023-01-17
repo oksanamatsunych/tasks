de8ab8df78ed79186963af72f79415b39f829a86
Oksana <106753310+oksanamatsunych@users.noreply.github.com>
2023-01-17 23:52:27 +0000

    Create hometask3.js

---
+++ hometask3.js
@@ -0,0 +1,9 @@
+const name = "iPhone";
+const oldModelVersion = 11;
+const oldModelUSDPrice = 799;
+const newModelVersion = 15;
+const newModelUSDPrice = 1999;
+const uahUSDRate = 29;
+
+const message = `Old ${this.name + oldModelVersion} cost ${oldModelUSDPrice} in 2019, ${this.name + newModelVersion} costs ${newModelUSDPrice} today. `
+console.log(message);
