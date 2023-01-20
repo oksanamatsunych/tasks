2dcc95d58b030dd359a9cd80d60815402388479f
Oksana <106753310+oksanamatsunych@users.noreply.github.com>
2023-01-20 19:10:30 +0000

    Update hometask3.js

--- hometask3.js
+++ hometask3.js
@@ -5,5 +5,11 @@ const newModelVersion = 15;
 const newModelUSDPrice = 1999;
 const uahUSDRate = 29;
 
-const message = `Old ${this.name + oldModelVersion} cost ${oldModelUSDPrice} in 2019, ${this.name + newModelVersion} costs ${newModelUSDPrice} today. `
-console.log(message);
+const message1 = `Old ${name} ${oldModelVersion} cost $ ${oldModelUSDPrice} in 2019, new ${name} ${newModelVersion} costs $ ${newModelUSDPrice} today. `;
+console.log(message1);
+
+const message2 = `New version of ${name} is ${newModelUSDPrice - oldModelUSDPrice} more expensive than previous `;
+console.log(message2);
+
+const message3 = `In Ukraine these prices are ${oldModelUSDPrice * 29} UAH and ${newModelUSDPrice * 29} UAH respectively`;
+console.log(message3);
